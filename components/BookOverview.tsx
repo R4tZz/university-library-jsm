29858659531f6bc193ee815c5ac974f1453d64d7
R4tZz <goncalo.moura@live.com.pt>
2025-02-27 00:08:35 +0100

    fix: bookoverview btn fix

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import BookCover from "./BookCover";
-import BorrowBook from "./BorrowBook";
+import BookCover from "@/components/BookCover";
+import BorrowBook from "@/components/BorrowBook";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
@@ -9,7 +9,6 @@ import { eq } from "drizzle-orm";
 interface Props extends Book {
   userId: string;
 }
-
 const BookOverview = async ({
   title,
   author,
@@ -29,53 +28,55 @@ const BookOverview = async ({
     .where(eq(users.id, userId))
     .limit(1);
 
-  if (!user) return null;
-
   const borrowingEligibility = {
-    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
     message:
       availableCopies <= 0
         ? "Book is not available"
         : "You are not eligible to borrow this book",
   };
-
   return (
     <section className="book-overview">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
+
         <div className="book-info">
           <p>
             By <span className="font-semibold text-light-200">{author}</span>
           </p>
+
           <p>
             Category{" "}
             <span className="font-semibold text-light-200">{genre}</span>
           </p>
+
           <div className="flex flex-row gap-1">
-            <Image
-              src="/icons/star.svg"
-              alt="star"
-              width={22}
-              height={22}
-            ></Image>
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
             <p>{rating}</p>
           </div>
         </div>
+
         <div className="book-copies">
           <p>
-            Total Books: <span>{totalCopies}</span>
+            Total Books <span>{totalCopies}</span>
           </p>
+
           <p>
-            Available Books: <span>{availableCopies}</span>
+            Available Books <span>{availableCopies}</span>
           </p>
         </div>
+
         <p className="book-description">{description}</p>
-        <BorrowBook
-          bookId={id}
-          userId={userId}
-          borrowingEligibility={borrowingEligibility}
-        />
+
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
+
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
           <BookCover
